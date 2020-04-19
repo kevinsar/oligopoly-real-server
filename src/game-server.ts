2769d8f9bcfafaf692dd66d00c7224dfe0dd6e34
Kevin Sar <kevin.sar@perficient.com>
2020-04-18 22:14:31 -0700

    Fix CORS issues.

--- src/game-server.ts
+++ src/game-server.ts
@@ -371,13 +371,12 @@ export class GameServer {
     this.app = express();
     this.app.use(function(req, res, next) {
       res.header('Access-Control-Allow-Origin', '*');
-      res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
-      );
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
       next();
     });
     this.app.use(bodyParser.json());
+
   }
 
   config(): void {
