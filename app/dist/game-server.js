b209d33518aa1af04b3f5b58ee9fc5c40176c2d5
Kevin Sar <kevin.sar@perficient.com>
2020-04-18 23:05:13 -0700

    deploy 2

--- app/dist/game-server.js
+++ app/dist/game-server.js
@@ -310,7 +310,7 @@ class GameServer {
     createApp() {
         this.app = express();
         this.app.use(function (req, res, next) {
-            res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+            res.header('Access-Control-Allow-Origin', '*');
             res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
             next();
         });
