c22518a68d42bc890ca9f1b2f36e485b1a8d5e00
Kevin Sar <kevin.sar@perficient.com>
2020-04-18 22:25:46 -0700

    Fix CORS issue 2

--- src/game-server.ts
+++ src/game-server.ts
@@ -370,13 +370,14 @@ export class GameServer {
   createApp(): void {
     this.app = express();
     this.app.use(function(req, res, next) {
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+      res.header('Access-Control-Allow-Origin', 'http://oligopoly.tableflip.games http://localhost:4200');
+      res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+      );
       next();
     });
     this.app.use(bodyParser.json());
-
   }
 
   config(): void {
